5946793c5c082f4ec3755699369d2737b603e47e
oghuz <osmanjan.omar@gmail.com>
2017-04-09 10:40:18 -0700

    test: testing methods calling order

--- MemeMeFirst/MemeMeEditorViewController.swift
+++ MemeMeFirst/MemeMeEditorViewController.swift
@@ -21,28 +21,27 @@ class MemeMeEditorViewController: UIViewController, UIImagePickerControllerDeleg
     @IBOutlet weak var topTextFieldOutlet: UITextField! {
         didSet {
             topTextFieldOutlet.delegate = self
-            self.configuerTextfields(textField: buttomTextFieldOutlet, enale: false)
         }
     }
+    
     @IBOutlet weak var buttomTextFieldOutlet: UITextField! {
         didSet {
             buttomTextFieldOutlet.delegate = self
-            self.configuerTextfields(textField: topTextFieldOutlet, enale: false)
         }
     }
-        
+    
     @IBOutlet weak var toolBarOutLet: UIToolbar!
     @IBOutlet weak var cemeraButtonOutlet: UIBarButtonItem!
     
- 
+    
     // creating text field atributes
-   let textFieldAtributes = [    
-                    NSFontAttributeName : UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
-                    NSForegroundColorAttributeName : UIColor.white,
-                    NSStrokeColorAttributeName : UIColor.black,
-                    NSStrokeWidthAttributeName: -3.5
-    ] as [String : Any]
-
+    let textFieldAtributes = [
+        NSFontAttributeName : UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSForegroundColorAttributeName : UIColor.white,
+        NSStrokeColorAttributeName : UIColor.black,
+        NSStrokeWidthAttributeName: -3.5
+        ] as [String : Any]
+    
     
     //#MARK: Cancel button action
     
@@ -50,16 +49,17 @@ class MemeMeEditorViewController: UIViewController, UIImagePickerControllerDeleg
     @IBAction func cancelButtonAction(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
     }
-        
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-                    }
+        self.configuerTextfields(textField: topTextFieldOutlet, enale: false)
+        self.configuerTextfields(textField: buttomTextFieldOutlet, enale: false)
+    }
     
     //#MARK: configure text fields
     func configuerTextfields(textField:UITextField, enale:Bool?){
-    
+        
         textField.defaultTextAttributes = textFieldAtributes
         textField.textAlignment = NSTextAlignment.center
         textField.isEnabled = enale!
@@ -69,15 +69,15 @@ class MemeMeEditorViewController: UIViewController, UIImagePickerControllerDeleg
         buttomTextFieldOutlet.attributedPlaceholder = NSAttributedString(string: "Buttom", attributes: [NSForegroundColorAttributeName:UIColor.white])
         
     }
- 
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
         // camera button disabled on the devices where caemras on available
         if !UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera){
-        
+            
             cemeraButtonOutlet.isEnabled = false
-        
+            
         }
         self.subscribeToKeyBoardWillShowNotification()
         self.subscribeToKeyBoardWillHideNotification()
@@ -94,22 +94,22 @@ class MemeMeEditorViewController: UIViewController, UIImagePickerControllerDeleg
     
     
     // # MARK: choose photo, take photo, share button actions, save Meme to library
-
-      
+    
+    
     @IBAction func avtivityButton(_ sender: AnyObject) {
         
         let sharedImage = self.makeMemedImage()
         let activityVC = UIActivityViewController(activityItems: [sharedImage], applicationActivities: nil)
         self.present(activityVC, animated: true, completion: nil)
-      
+        
         activityVC.completionWithItemsHandler = {
-        (activityType, completed, returnedItems, activityError) in
-        if completed {
-        self.saveMemeObject(memeImage: sharedImage)
-        self.dismiss(animated: true, completion: nil)
-        }
-            
+            (activityType, completed, returnedItems, activityError) in
+            if completed {
+                self.saveMemeObject(memeImage: sharedImage)
+                self.dismiss(animated: true, completion: nil)
             }
+            
+        }
     }
     
     
@@ -130,14 +130,14 @@ class MemeMeEditorViewController: UIViewController, UIImagePickerControllerDeleg
     // # MARK: implementing pickImageFromSource
     
     func pickImageFromSource(source:UIImagePickerControllerSourceType){
-    
+        
         let imagePickerVC = UIImagePickerController()
         imagePickerVC.delegate = self
         imagePickerVC.sourceType = source
         self.present(imagePickerVC, animated: true, completion: nil)
         
-    
-    
+        
+        
     }
     
     // # MARK: UIImagePickerViewController delegate methods
@@ -158,17 +158,17 @@ class MemeMeEditorViewController: UIViewController, UIImagePickerControllerDeleg
         self.dismiss(animated: true, completion: nil)
         
     }
-
+    
     
     // creating alert method for reuse
     func alert(atitle:String, amessage:String, aactionTitle:String, aactionStyle:UIAlertActionStyle)->UIAlertController{
-    
+        
         let aAlert = UIAlertController(title: atitle, message: amessage, preferredStyle: UIAlertControllerStyle.alert)
         
         let cancel = UIAlertAction(title:aactionTitle, style: aactionStyle, handler: { (UIAlertAction) in})
         aAlert.addAction(cancel)
-               return aAlert
-    
+        return aAlert
+        
     }
     
     
@@ -180,10 +180,10 @@ class MemeMeEditorViewController: UIViewController, UIImagePickerControllerDeleg
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
         self.view.endEditing(true)
-     
+        
     }
- 
-
+    
+    
     // hide keyboard when pressing return key "Done"
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -205,77 +205,77 @@ class MemeMeEditorViewController: UIViewController, UIImagePickerControllerDeleg
         
         return keyBoardHeight.cgRectValue.height
     }
-
-  
+    
+    
     
     // subscribing the keyboardWillShowUp method
     
     func subscribeToKeyBoardWillShowNotification(){
-    
+        
         NotificationCenter.default.addObserver(self, selector: #selector(theKeyboardWillShowUp), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
         
     }
     
     // unsubscribe from keyboard show notification
     func unsubscribeFromKeyboardWillShowNotification(){
-    
+        
         NotificationCenter.default.removeObserver(self, name: Notification.Name.UIKeyboardWillShow, object: nil)
-           }
-
+    }
+    
     // move up the buttom textfield and pull down the top textfield when keyboard show up
     func theKeyboardWillShowUp(notification:NSNotification){
         
         if buttomTextFieldOutlet.isFirstResponder{
-               view.frame.origin.y -= theKeyBoardHeight(notification: notification)
+            view.frame.origin.y -= theKeyBoardHeight(notification: notification)
         }
         
     }
     
-     // # MARK: moving down the view when keyborad hide
+    // # MARK: moving down the view when keyborad hide
     
     // subscribe to the UIKeyboardWillHide notification
     
     func subscribeToKeyBoardWillHideNotification(){
-    
+        
         NotificationCenter.default.addObserver(self, selector: #selector(moveDownTheView), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
-    
+        
     }
     
     //unsubscribe from the notification
     func unsubscribeFromKeyBoardWillHideNotification(){
-    
+        
         NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide, object: nil)
-    
+        
     }
     
     // set the y origin to original point
     func moveDownTheView(notification:NSNotification){
-    
+        
         if buttomTextFieldOutlet.isFirstResponder{
-      view.frame.origin.y += theKeyBoardHeight(notification: notification)
+            view.frame.origin.y += theKeyBoardHeight(notification: notification)
         }
     }
-
+    
     //#MARK: saving Meme object with photos and texts
-
+    
     
     func saveMemeObject(memeImage:UIImage){
         
         let Meme = MeMeme(originalImage : imageViewOutlet.image,
-                              memeImage : self.makeMemedImage(),
-                                topText : topTextFieldOutlet.text,
-                             buttonText : buttomTextFieldOutlet.text)
+                          memeImage : self.makeMemedImage(),
+                          topText : topTextFieldOutlet.text,
+                          buttonText : buttomTextFieldOutlet.text)
         
         let object = UIApplication.shared.delegate
         let appdelegate = object as! AppDelegate
         appdelegate.Memes.append(Meme)
-    
+        
     }
     
     //#MARK: capturing the screen as image after finish editing MeMe
     
     func makeMemedImage()->UIImage{
-    
+        
         
         toolBarOutLet.isHidden = true
         
@@ -288,7 +288,7 @@ class MemeMeEditorViewController: UIViewController, UIImagePickerControllerDeleg
         
         
         return memedImage!
-
+        
     }
     
     
