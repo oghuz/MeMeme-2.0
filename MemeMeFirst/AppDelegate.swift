8abf5d1945707e2870bb5ab53458f27f5905b66c
Wusiman Wumaier <hizmatla@gmail.com>
2018-01-17 12:28:48 -0800

    unRegisterNSManagedObjectContextdidChangeNotification

--- MemeMeFirst/AppDelegate.swift
+++ MemeMeFirst/AppDelegate.swift
@@ -58,7 +58,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         saveContext()
         
-        unRegisterNSManagedObjectContextdidChangeNotification()
+        //unRegisterNSManagedObjectContextdidChangeNotification()
         //unRegisterNSManagedObjectContextdidFinishSaveNotification()
         
     }
